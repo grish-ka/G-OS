a2d930424dddddc8b765a0e3f23a686a1fbe8c40
grish-ka <grishka.krot@gmail.com>
2025-02-10 17:49:48 +0000

    rethink the os

--- src/bootloader/stage2/stdint.h
+++ src/bootloader/stage2/stdint.h
@@ -1,8 +1,15 @@
 #pragma once
-#include <stdint.h>
 
-void clrscr();
-void putc(char c);
-void puts(const char* str);
-void printf(const char* fmt, ...);
-void print_buffer(const char* msg, const void* buffer, uint32_t count);
+typedef signed char int8_t;
+typedef unsigned char uint8_t;
+typedef signed short int16_t;
+typedef unsigned short uint16_t;
+typedef signed long int int32_t;
+typedef unsigned long int uint32_t;
+typedef signed long long int int64_t;
+typedef unsigned long long int uint64_t;
+
+typedef uint8_t bool;
+
+#define false 0
+#define true 1
